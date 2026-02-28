c725bd3bc660b691182a0be067bd3783bcac1905
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2026-02-28 18:43:17 +0800

    feat: created the game logic of tracking the clicked cards, the current and best score and reshuffling the cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react';
 import Card from './components/Card';
 import generateRandomNumbers from './utils/generateRandomNumbers';
+import shuffleArray from './utils/shuffleArray';
 import './styles/App.css';
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
+  const [clickedPokemonIds, setClickedPokemonIds] = useState([]);
   const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
 
   useEffect(() => {
     // Asynchronous function to help to make 5 API calls on 5 random IDs of pokemon
@@ -34,15 +37,43 @@ function App() {
   }, []);
 
   const handleClick = (e) => {
-    setCurrentScore(currentScore + 1);
+    // Obtain the id the pokemon clicked
+    const clickedPokemonId = e.currentTarget.id;
+
+    // If the id of the pokemon clicked has already been clicked, reset the game by resetting the current score and removing all the ids in the clickedPokemonIds
+    if (clickedPokemonIds.includes(clickedPokemonId)) {
+      setCurrentScore(0);
+      setClickedPokemonIds([]);
+    } else {
+      // Else if it has not been clicked
+      setCurrentScore((prevScore) => {
+        // Increment current score
+        const newScore = prevScore + 1;
+
+        // To prevent the best score updating in the re render, wrap it within the setCurrentScore
+        // Change the best score if the current score is greater than the new score
+        setBestScore((prevBestScore) => (newScore > prevBestScore ? newScore : prevBestScore));
+
+        return newScore;
+      });
+
+      // Add the pokemon id to the clicked ids
+      setClickedPokemonIds((prevClickedPokemonIds) => [...prevClickedPokemonIds, clickedPokemonId]);
+    }
+
+    // Reshuffle the pokemon list
+    setPokemonList((prevPokemonList) => shuffleArray(prevPokemonList));
   };
 
   return (
-    <div className="flex flex-col justify-center">
+    <div className="grid grid-rows-2 justify-center">
       <h1 className="text-3xl font-bold">Current score: {currentScore}</h1>
-      {pokemonList.map((pokemon) => (
-        <Card key={pokemon.id} {...pokemon} handleClick={handleClick} />
-      ))}
+      <h1 className="text-3xl font-bold">Best score: {bestScore}</h1>
+      <div className="flex gap-2">
+        {pokemonList.map((pokemon) => (
+          <Card key={pokemon.id} {...pokemon} handleClick={handleClick} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ id, name, spriteUrl, handleClick }) {
   return (
-    <div key={id} onClick={handleClick}>
+    <div key={id} id={id} onClick={handleClick} className="border border-gray-200">
       <h1>{name}</h1>
       <img src={spriteUrl} />
     </div>
